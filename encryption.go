68d4d0409584edf9b809715033cc8c9ff34082b8
凌海仲子 <ysg@myschools.me>
2022-08-15 22:21:24 +0800

    服务端加解密

---
+++ encryption.go
@@ -0,0 +1,41 @@
+package godingtalk
+
+import "encoding/json"
+
+// DataMessage 服务端加密、解密消息
+type DataMessage struct {
+	OAPIResponse
+	Data string
+}
+
+// Encrypt is 服务端加密
+func Encrypt(str string) (string, error) {
+	var data DataMessage
+	request := map[string]interface{}{
+		"data": str,
+	}
+	payload, err := httpRequest("encryption/encrypt", nil, request)
+	if err != nil {
+		return "", err
+	}
+	if err := json.Unmarshal(*payload, &data); err != nil {
+		return "", err
+	}
+	return data.Data, nil
+}
+
+// Decrypt is 服务端解密
+func Decrypt(str string) (string, error) {
+	var data DataMessage
+	request := map[string]interface{}{
+		"data": str,
+	}
+	payload, err := httpRequest("encryption/decrypt", nil, request)
+	if err != nil {
+		return "", err
+	}
+	if err := json.Unmarshal(*payload, &data); err != nil {
+		return "", err
+	}
+	return data.Data, nil
+}
